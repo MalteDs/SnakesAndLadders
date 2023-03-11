c6a0905a60842f801ca69ef0ba8787964539cb25
John Hortua <1109660285@u.icesi.edu.co>
2023-03-10 23:43:20 -0500

    Changed the names of the fields and the name of certain methods respectively. Added the method 'move'.

--- src/model/Player.java
+++ src/model/Player.java
@@ -1,11 +1,30 @@
 package model;
 
 public class Player {
+	private int position;
+	private final char symbol;
 
-    private char id;
-    private int pos = 1;
+	public Player(int startingPosition, char symbol) {
+		this.position = startingPosition;
+		this.symbol = symbol;
+	}
 
-    public Player(char id) {
-        this.id = id;
-    }
+	public int getPosition() {
+		return position;
+	}
+
+	public void setPosition(int position) {
+		this.position = position;
+	}
+
+	public char getSymbol() {
+		return symbol;
+	}
+
+	public void move(int steps) {
+		position += steps;
+	}
 }
+
+
+
