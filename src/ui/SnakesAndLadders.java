ff8daf486a872a4111ea92dd93cdcae87dbd14cd
John Hortua <1109660285@u.icesi.edu.co>
2023-03-10 23:46:26 -0500

    Changed the file name from 'Main' to 'SnakesAndLadders'. Changed the name of 'createPlayer' to 'addPlayer' and 'restartGame' to 'resetGame'. Added the methods 'displayBoard', 'displayScore' and 'setScore'

--- src/ui/SnakesAndLadders.java
+++ src/ui/SnakesAndLadders.java
@@ -1,7 +1,31 @@
 package ui;
+import java.time.Instant;
+import java.time.Duration;
+import java.util.Scanner;
+import model.Game;
 
 public class SnakesAndLadders {
+
+    public static Instant i;
+    public static Duration d;
+    public static Scanner sc = new Scanner(System.in);
+    public static Game game;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
     }
+
+    public static void startGame(){}
+
+    public static void endGame(){}
+
+    public static void displayBoard(){}
+
+    public static void setScore(){}
+
+    public static void displayScore(){}
+
+    public static void addPlayer(){}
+
+    public static void resetGame(){}
 }
