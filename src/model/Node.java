db0114e124db36077b2724236544ce773fd9ee18
John Hortua <1109660285@u.icesi.edu.co>
2023-03-10 23:28:44 -0500

    Now Node is an abstract class. Changed the fields: Node next, Player[] players. Modified all the methods, now having the following: 'getNext', 'setNext', 'getPlayer', 'setPlayer', 'getPlayerSymbol', 'addPlayers', 'getPlayers'

--- src/model/Node.java
+++ src/model/Node.java
@@ -1,9 +1,40 @@
 package model;
 
-public class Node {
+public abstract class Node {
 
-    private Node x;
-    private Node y;
+    protected Node next;
     private Player player;
-    private int id;
+	private Player[] players = new Player[3];
+
+	public Node getNext() {
+		return next;
+	}
+
+	public void setNext(Node next) {
+		this.next = next;
+	}
+
+	public Player getPlayer() {
+		return player;
+	}
+
+	public void setPlayer(Player player) {
+		this.player = player;
+	}
+
+	public char getPlayerSymbol() {
+		return player.getSymbol();
+	}
+
+	public void addPlayers(Player player, int index) {
+		players[index] = player;
+	}
+
+	protected Player[] getPlayers() {
+		return players;
+	}
+
+
+
 }
+
